4847adccbec3aa77787e157c6e2095d78dd1f62a
Azhar Ibn Mostafiz <theazharul@gmail.com>
2015-06-29 15:52:27 +0600

    data folder deleted and some structure changed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'app/themes/default'));
+app.set('views', path.join(__dirname, 'app/views/default'));
 app.set('view engine', 'jade');
 app.enable ('view cache');
 
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 require('./app/routes')(app);
 
